d27ed244e2524f422d1a1740822f1697d8e9001f
Manuel Sousa <mlrcbsousa@gmail.com>
2024-12-08 19:58:04 +0100

    testing

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use matrix;
+
+fn exercise_00() {
+	println!();
+	println!("Exercise 00 - Add, Subtract and Scale");
+	println!("Testing basic hello world");
+	println!("{}", matrix::say_hello());
+}
+
+fn main() {
+	println!("Matrix Library Demo");
+	println!("===================");
+
+	exercise_00();
+}
